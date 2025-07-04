f3a66fd1068979eba7b406e48662aadad2475009
RubenOpperman <Oppiesfam123@gmail.com>
2025-07-04 18:33:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-blue-500">hello</h1>
+    </>
+  );
+}
+
+export default App;
